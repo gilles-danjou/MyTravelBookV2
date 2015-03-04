052230131cf63d15878e9c5f1a78d948dcc70bf7
Gilles Danjou <gilles.danjou@gmail.com>
2015-03-04 11:45:02 +0100

    masonry for articles
    Issue: Fetch doesn’t display articles on hidden tab

--- client/search/search.ctrl.js
+++ client/search/search.ctrl.js
@@ -1,7 +1,62 @@
 
-angular.module('searchCtrl', ['searchService'])
+angular.module('searchCtrl', ['searchService', 'articleService', 'masonryLayout', 'infinite-scroll'])
 
-    .controller('searchController', ['$rootScope', '$scope', 'Search', function($rootScope, $scope, Search) {
+.directive('myCustomer', function() {
+    return {
+        link: function (scope, element) {
+            scope.fetchNext();
+            debugger
+        }
+    };
+})
+
+.controller('searchController', ['$rootScope', '$scope', 'Search', 'Article', '$interval', '$timeout', function($rootScope, $scope, Search, Article, $interval, $timeout) {
+
+        var busy = false,
+            heights = [254, 300, 192, 450, 350, 200, 360, 420, 270, 400, 200],
+            cats = ['abstract', 'animals', 'business', 'cats', 'city', 'food', 'nightlife', 'fashion', 'people', 'nature', 'sports', 'transport'],
+            stop;
+
+        $scope.images = [];
+
+        $scope.refresh = function() {
+            $scope.images.length = 0;
+
+            $timeout(function() { $scope.fetchNext(); }, 1000);
+        };
+
+        $scope.fetchNext = function() {
+            var i=0;
+
+            if(!busy) {
+                console.log('Fetching next')
+                busy = true;
+                Article.all().success(function(articles) { vm.processing = false; vm.allArticles = articles;
+
+                    _.each(articles, function(article) {
+                        $scope.images.push({
+                            src: '250/' + heights[i % heights.length] + '/' + cats[Math.floor(Math.random() * cats.length)],
+                            title: article.info.imageTitle
+                        });
+                    });
+                    busy = false;
+
+                });
+
+                //for(var i=0; i<30; i++) {
+                //    $scope.images.push({
+                //        src: '250/' + heights[i % heights.length] + '/' + cats[Math.floor(Math.random() * cats.length )]
+                //    });
+                //}
+
+                //busy = false;
+
+            }
+
+        };
+
+
+
         var vm = this;
         vm.type = 'create';
         updatePageTitle('Find a destination');
