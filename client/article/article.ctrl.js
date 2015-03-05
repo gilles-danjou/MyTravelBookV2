d6ea53edd8d07e699ef6a200c4dc59ddb5a00bd6
Gilles Danjou <gilles.danjou@gmail.com>
2015-03-05 18:05:17 +0100

    weather

--- client/article/article.ctrl.js
+++ client/article/article.ctrl.js
@@ -1,4 +1,4 @@
-angular.module('articleCtrl', ['articleService'])
+angular.module('articleCtrl', ['articleService', 'masonryLayout', 'infinite-scroll'])
 
 //
 //.directive('onFinishRender', function ($timeout) {
@@ -28,11 +28,47 @@ angular.module('articleCtrl', ['articleService'])
             }
         });
     });
-        
+
     // grab all the articles at page load
 	Article.all().success(function(data) { vm.articles = data; vm.processing = false; });
 
-}]);
+}])
 
 
+.controller('articleMasonryController', ['$rootScope', '$scope', 'Search', 'Article', '$interval', '$timeout', function($rootScope, $scope, Search, Article, $interval, $timeout) {
 
+    var vm = this;
+    vm.processing = true;
+
+        var busy = false,
+        heights = [254, 300, 192, 450, 350, 200, 360, 420, 270, 400, 200],
+        cats = ['abstract', 'animals', 'business', 'cats', 'city', 'food', 'nightlife', 'fashion', 'people', 'nature', 'sports', 'transport'],
+        stop;
+
+    $scope.images = [];
+
+    $scope.refresh = function() {
+        $scope.images.length = 0;
+
+        $timeout(function() { $scope.fetchNext(); }, 1000);
+    };
+
+    $scope.fetchNext = function() {
+        var i=0;
+
+        if(!busy) {
+            console.log('Fetching next')
+            busy = true;
+            Article.all().success(function(articles) { vm.processing = false; vm.allArticles = articles;
+
+                _.each(articles, function(article) {
+                    $scope.images.push({
+                        src: '250/' + heights[i % heights.length] + '/' + cats[Math.floor(Math.random() * cats.length)],
+                        title: article.info.imageTitle
+                    });
+                });
+                busy = false;
+            });
+        }
+    };
+}])
